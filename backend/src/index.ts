65cdfd2b80e0ba1924e888e22c1d0527418ef0ca
SarthakJain29 <sarthakjain2907@gmail.com>
2024-03-19 18:28:06 +0530

    initialize

---
+++ backend/src/index.ts
@@ -0,0 +1,43 @@
+import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const app = new Hono();
+
+app.post('/api/v1/signup', async (c) => {
+	const prisma = new PrismaClient({
+		//@ts-ignore
+		datasourceUrl: env.DATABASE_URL,
+	}).$extends(withAccelerate())
+	
+	const body = await c.req.json();
+
+	await prisma.user.create({
+		data:{
+			email: body.email,
+			password: body.password,
+		},
+	})
+	return c.text('signup route')
+})
+
+app.post('/api/v1/signin', (c) => {
+	return c.text('signin route')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+	const id = c.req.param('id')
+	console.log(id);
+	return c.text('get blog route')
+})
+
+app.post('/api/v1/blog', (c) => {
+
+	return c.text('signin route')
+})
+
+app.put('/api/v1/blog', (c) => {
+	return c.text('signin route')
+})
+
+export default app;

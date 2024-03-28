c2d2e1f2e7c99af5450e962bf6ebec58b81e0381
SarthakJain29 <sarthakjain2907@gmail.com>
2024-03-28 20:46:04 +0530

    final

--- frontend/src/components/fullblog.tsx
+++ frontend/src/components/fullblog.tsx
@@ -1,32 +1,44 @@
-import { Blog } from "../hooks";
-import { Appbar } from "./appbar";
-import { Avatar } from "./blogcard";
+import { Blog } from "../hooks"
+import { Appbar } from "./appbar"
+import { Avatar } from "./blogcard"
 
 
-export const FullBlog = ({ blog }: { blog: Blog }) => {
-    return (
-        <div>
-            <Appbar />
-            <div className="flex justify-center">
-            <div className="grid grid-cols-4 pt-10 w-8/12">
-                <div className="col-span-3">
-                    <div className="text-4xl font-bold pb-2">{blog.title}</div>
-                    <div className="pb-4 text-slate-600">Posted on 23 March, 2025</div>
-                    <div>{blog.content}</div>
-                </div>
-                <div className="col-span-1 pl-20">
-                    <div className="pb-4 text-slate-500">Author</div>
-                    <div className="flex">
-                        <div className="flex flex-col justify-center">
-                            <Avatar name={blog.author.name}/>
-                        </div>
-                        <div className="px-3 text-xl font-bold max-h-full flex flex-col justify-center">
-                            {blog.author.name || "Anonymous"}
-                        </div>
+export const FullBlog = ({ blog }: {blog: Blog}) => {
+    return <div>
+        <Appbar />
+        <div className="flex justify-center">
+            <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
+                <div className="col-span-8">
+                    <div className="text-5xl font-extrabold">
+                        {blog.title}
+                    </div>
+                    <div className="text-slate-500 pt-2">
+                        Post on 2nd December 2023
+                    </div>
+                    <div className="pt-4">
+                        {blog.content}
                     </div>
                 </div>
+                <div className="col-span-4">
+                    <div className="text-slate-600 text-lg">
+                        Author
+                    </div>
+                    <div className="flex w-full">
+                        <div className="pr-4 flex flex-col justify-center">
+                            <Avatar name={blog.author.name || "Anonymous"} />
+                        </div>
+                        <div>
+                            <div className="text-xl font-bold">
+                                {blog.author.name || "Anonymous"}
+                            </div>
+                            <div className="pt-2 text-slate-500">
+                                Random catch phrase about the author's ability to grab the user's attention
+                            </div>
+                        </div>
+                    </div>  
+                </div>
+                
             </div>
         </div>
     </div>
-    );
-};
+}
